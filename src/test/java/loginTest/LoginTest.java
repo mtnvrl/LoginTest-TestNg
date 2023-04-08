978ed8b1095b4b17214d417f1c961e1c62f4323d
VMetin <vametin@gmail.com>
2023-04-08 14:48:51 +0300

    LT

---
+++ src/test/java/loginTest/LoginTest.java
@@ -0,0 +1,14 @@
+package loginTest;
+
+import org.junit.Test;
+import utilities.TestBase;
+
+public class LoginTest extends TestBase {
+    @Test
+
+    public void loginTest01 (){
+        driver.get("https://www.amazon.com");
+
+
+    }
+}
---
+++ src/test/java/utilities/TestBase.java
@@ -0,0 +1,26 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+    public abstract class TestBase {
+        protected WebDriver driver;
+        @Before
+        public void setUp(){
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options=new ChromeOptions();
+            options.addArguments("--remote-allow-origins=*");
+            driver=new ChromeDriver(options);
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        }
+        @After
+        public void tearDown(){
+             driver.quit();
+        }
+}
